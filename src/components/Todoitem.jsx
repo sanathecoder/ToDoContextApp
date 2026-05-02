f8df4b09ccecfb5e284268e202bad9093db73e5b
sanathecoder <sanasana33377737@gmail.com>
2026-05-02 22:18:42 +0500

    change color

--- src/components/Todoitem.jsx
+++ src/components/Todoitem.jsx
@@ -19,7 +19,7 @@ function Todoitem({ todo }) {
   return (
          <div
             className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
-                todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
+                todo.completed ? "bg-[#c6e9a7]" : "bg-[#bed7cc]"
             }`}
         >
             <input
